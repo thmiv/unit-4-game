893c965ff346cdd016cfccb3b9e6973d68767e32
Thomas <thmiv4@gmail.com>
2018-07-31 15:04:05 -0500

    functioning game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,13 @@ var currentScore;
 var wins = 0;
 var losses = 0;
 
+function updateDisplay() {
+  $("#winning-number").text(winningNumber);
+  $("#current-score").text(currentScore);
+  $("#num-wins").text(wins);
+  $("#num-loss").text(losses);
+}
+
 function crystalValueGenerator() {
   crystalOne = Math.floor(Math.random() *(12 - 1 + 1)) + 1;
   crystalTwo = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
@@ -23,7 +30,36 @@ function newGame() {
   console.log(winningNumber);
   currentScore = 0;
   crystalValueGenerator();
+  updateDisplay();
+}
+function checkForWin() {
+  updateDisplay();
+  if (currentScore === winningNumber){
+    alert("YOU WINNER!");
+    wins++;
+    newGame();
+  } else if (currentScore > winningNumber) {
+    alert("You're loser!");
+    losses++;
+    newGame();
+  }
 }
 
 newGame();
 
+$("#blue").on("click", function() {
+  currentScore += crystalOne;
+  checkForWin();
+});
+$("#red").on("click", function() {
+  currentScore += crystalTwo;
+  checkForWin();
+});
+$("#green").on("click", function() {
+  currentScore += crystalThree;
+  checkForWin();
+});
+$("#purple").on("click", function() {
+  currentScore += crystalFour;
+  checkForWin();
+});
